3e9c77e8b9a03a0270f76b07853517830beba769
Okunoluwakiikuna Enoch Emmanuel <okunoluwa2019@gmail.com>
2025-08-12 16:18:30 +0200

    Refactor print statements in python2.py for clarity and consistency

--- python2.py
+++ python2.py
@@ -1,3 +1,26 @@
 #print('Hickory Dickory Dock! The mouse ran up the clock')
 print('The capybara is the worlds largest rodent')
 print('The capybara likes to live in group')
+print("The capybara can swim")
+print("It's a beautiful day in the neighborhood")
+print("Hickory Dickory Dock!\nThe mouse ran uo the clock")
+print("""Hickory Dickory Dock
+The mouse ran up the clock""")
+print('''Hickory Dickory Dock
+The mouse ran up the clock''')
+#demo(
+print("the capybara lives in \nSouth America")
+print("""This is the strangest 
+way to print over multiple lines I know""")
+
+print("here is a string" + " here is more")
+print('here is a double quote "' + " here is a single quote '")
+print("or you can just do this \" does this work")
+
+# is to add spaces
+print("can i just print a \t on the screen")
+#nothing special
+print("can i print a \ on the scree")
+
+#i am inserting a \\ so the \ appears correctly in the string 
+print("But what if I want \\news ")
